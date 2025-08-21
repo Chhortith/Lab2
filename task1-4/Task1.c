a60ea4f00d85b46600b59a9290888b58b79498a5
Chhortith <674244130@mail.pbru.ac.th>
2025-08-21 12:47:16 +0700

    Add files via upload

---
+++ task1-4/Task1.c
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main() {
+    printf("Current Process ID: %lu\n", GetCurrentProcessId());
+    printf("Current Thread ID : %lu\n", GetCurrentThreadId());
+    return 0;
+}
